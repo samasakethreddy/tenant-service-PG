f2afd3d8a5d3a3878860b94c68c04f5d90f2c5fd
samasakethreddy <samasakethreddy716@gmail.com>
2025-03-06 21:05:35 +0530

    initial

--- src/main/java/com/tenants/tenants_service/MainTenantsApplication.java
+++ src/main/java/com/tenants/tenants_service/MainTenantsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainTenantsApplication {
-
 	public static void main(String[] args) {
 		System.out.println("Tenant micro-service started...");
 		SpringApplication.run(MainTenantsApplication.class, args);
