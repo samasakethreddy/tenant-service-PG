ac3e46edc1c12c886985c094b390ad57bc329d60
samasakethreddy <samasakethreddy716@gmail.com>
2025-03-06 20:24:48 +0530

    initial

--- src/main/java/com/tenants/tenants_service/MainTenantsApplication.java
+++ src/main/java/com/tenants/tenants_service/MainTenantsApplication.java
@@ -2,11 +2,6 @@ package com.tenants.tenants_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 public class MainTenantsApplication {
